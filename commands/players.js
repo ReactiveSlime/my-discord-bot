28a3caeaf634a432e136547540e6b3ce5b335006
Vortex-Network <39306849+AtomicGaming666@users.noreply.github.com>
2022-06-12 12:51:45 +1000

    Revort

--- commands/players.js
+++ commands/players.js
@@ -9,65 +9,6 @@ const fetch = require('node-fetch');
 var botconfig = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-
-
-
-    if (args[0] == "" || args[0] == null) return 
-    fetch(`${botconfig.url}` + `/v1` + `/players`, {
-        method: "get",
-        headers: { "Content-Type": "application/json", "key": `${botconfig.key}` }
-    })
-        .then((res) => res.json())
-        .then((json) => {
-            // embed
-            let embed = new MessageEmbed();
-            embed.setTitle("Players");
-            embed.setColor("#0099ff");
-            for (var i = 0; i < json.length; i++) {
-                embed.addField(
-                    `${json[i].displayName}`,
-                    [
-                        `**❯ UUID:** ${json[i].uuid}`
-                    ].join('\n'),
-                )
-            }
-            message.channel.send({ embeds: [embed] })
-        })
-
-
-    if (args[0] == "uuid") {
-            fetch('https://api.mojang.com/user/profiles/' + uuid + '/names');
-            if (uuid == false) {
-                channel.message.send("The UUID provided is invalid.");
-                exit;
-            } else {
-                channel.message.send("The UUID provided is valid.");
-                exit;
-            }
-            exit;
-        } else {
-
-            message.reply("You are not in any Voice Channels, make sure you inside voice channel")
-
-        };
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
     fetch(`${botconfig.url}` + `/v1` + `/players`, {
         method: "get",
         headers: { "Content-Type": "application/json", "key": `${botconfig.key}` }
@@ -89,7 +30,6 @@ module.exports.run = async (client, message, args) => {
             message.channel.send({ embeds: [embed] })
         })
 };
-*/
 
 
 
