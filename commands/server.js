bf0ee2f711127ed11574110d7b1922f3ddffcab7
Vortex-Network <39306849+AtomicGaming666@users.noreply.github.com>
2022-06-12 12:17:38 +1000

    Updated Server info command

--- commands/server.js
+++ commands/server.js
@@ -18,21 +18,23 @@ module.exports.run = async (client, message, args) => {
     })
         .then((res) => res.json())
         .then((json) => {
+
+            fetch(`${botconfig.url}` + `/v1` + `/worlds/eb097c21-424c-43c3-8282-7cbb20e4a303`, {
+                method: "get",
+                headers: { "Content-Type": "application/json", "key": `${botconfig.key}` }
+            })
+                .then((res) => res.json())
+                .then((json2) => {
+                    if (json2.storm) {
+                        if (json2.thundering) {var weather = "Thundering Storm";}
+                        else { var weather = "Storming"; }}
+                    else { var weather = "Clear"; }
             //set uptime math
             var uptime = json.health.uptime;
             var days = Math.floor(uptime / 86400);
             var hours = Math.floor((uptime % 86400) / 3600);
             var minutes = Math.floor((uptime % 3600) / 60);
             var seconds = Math.floor(uptime % 60);
-
-            //set math for RAM use
-            //convort data from json.health.maxMemory from MB to GB
-            //free memory is used memory - total memory
-            /*
-            var totalMemory = json.health.maxMemory / 1024;
-            var freeMemory = json.health.freeMemory / 1024;
-            var usedMemory = totalMemory - freeMemory;
-            */
 
 
 
@@ -42,14 +44,10 @@ module.exports.run = async (client, message, args) => {
             embed.setTitle("Server Information")
             embed.setColor("#00ff00")
             embed.addField(`Server MODT`, `${json.motd}`)
+            embed.addField("Weather", `${weather}`, true);
             embed.addField(`TPS`, `${json.tps}`)
             embed.addField(`Uptime`, `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`)
-            
-            /*
-            embed.addField(`Total Memory`, `${totalMemory} GB`)
-            embed.addField(`Used Memory`, `${usedMemory} GB`)
-            embed.addField(`Free Memory`, `${freeMemory} GB`)
-            */
+
             message.channel.send({
                 embeds: [embed]
             })
