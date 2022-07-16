500fc0353aafbfbb30bc28d28429291f275e97e3
Vortex-Network <39306849+AtomicGaming666@users.noreply.github.com>
2022-07-16 22:14:00 +1000

    test

--- commands/test3.js
+++ commands/test3.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "about",
+    name: "test3",
     description: "About the bot",
     cooldown: 5,
 };
@@ -10,43 +10,15 @@ var botconfig = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
 
-    const version = "1.0.0";
-    //get bots created date
-    const CreatedDate = new Date(message.client.user.createdAt);
-    //get discord.js version
-    const DiscordJSVersion = require("discord.js").version;
-    //look on github for the latest release and get the version number and if its grater than the current version then show a new release message
-    // embed
-    let embed = new MessageEmbed();
-    embed.setTitle("Bot Info");
-    embed.setColor("#0099ff");
-    embed.addField(
-    "General",
-    [
-        "**❯ Developer:** [BallisticOK](https://atomicgaming666.com) / [ReactiveSlime](https://reactivesli.me)",
-        `**❯ Creation Date:** ${CreatedDate}`,
-        `**❯ Node.js:** ${process.version}`,
-        `**❯ Discord.js:** v${DiscordJSVersion}`,
-        `**❯ Github:** https://github.com/ReactiveSlime/my-discord-bot/`
-    ].join("\n")
-    );
-        //look on github for the latest release and get the version number and if its grater than the current version then show a new release message
-        fetch(`https://api.github.com/repos/ReactiveSlime/my-discord-bot/releases/latest`)
+    fetch(`https://api.github.com/repos/ReactiveSlime/my-discord-bot/releases/latest`)
         .then((res) => res.json())
         .then((json) => {
-            if (json.tag_name > version) {
-                embed.addField(
-                    "**❯ New Release:**",
-                    `**❯ Version:** ${json.tag_name}`,
-                    `**❯ Link:** ${json.html_url}`
-                );
-            }
+            console.log(json)
         }
         )
-        message.channel.send({ embeds: [embed] });
 }
 
 module.exports.help = {
-    name: "about",
+    name: "test3",
     description: "About the bot",
 };
